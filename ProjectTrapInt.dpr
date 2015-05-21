1b87ef769683073dd7364a87c906fb63c5bf75bc
Denis Stepulenok <super.denis@gmail.com>
2015-05-21 03:17:59 +0300

    Delphi implementation

---
+++ ProjectTrapInt.dpr
@@ -0,0 +1,57 @@
+﻿Program ProjectTrapInt;
+
+// Вычисление значения интеграла по методу трапеций
+// Программист Автор А.Ф.
+
+{$AppType Console}
+
+Uses SysUtils, Windows;
+
+var
+  s1, s2, // Рабочие переменные
+  x, // Переменная интегрирования
+  a, b: real; // Пределы интегрирования
+  n, k: integer; // Число разбиений, параметр цикла
+  eps, eps1, h: real; // Погрешность, шаг интегрирования
+
+function f(x: real): real;
+begin
+  result := 1.0 / sqrt(2 * x * x + 0.3);
+end;
+
+function Prompt(ps: PWideChar): PAnsiChar;
+var
+  bf: PAnsiChar;
+begin
+  GetMem(bf, Length(ps));
+  CharToOem(ps, bf);
+  result := bf;
+end;
+
+begin
+  Write('Введите границы отрезка,' + #10'число разбиений и погрешность: ');
+  ReadLn(a, b, n, eps);
+  eps1 := 10 * eps;
+  s2 := 0;
+  while eps1 > eps do
+  begin // R1
+    s1 := s2;
+    s2 := 0.5 * (f(b) + f(a));
+    h := (b - a) / n;
+    x := a + h;
+
+    for k := 1 to n - 1 do
+    begin
+      s2 := s2 + f(x);
+      x := x + h;
+    end;
+    eps1 := h * abs(s2 - 2 * s1) / 3;
+    // Для просмотра динамики вычисления интеграла
+    WriteLn('N = ', n, ' eps1 = ', eps1, #10' Int = ', h * s2);
+    // ReadLn;
+    n := 2 * n;
+  end; // R1
+
+  WriteLn(' Значение интеграла: ', h * s2:12, #10' с точностью ', eps:12);
+  ReadLn;
+end.
